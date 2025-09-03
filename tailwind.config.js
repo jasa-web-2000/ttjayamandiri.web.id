c92d29e5ec96635a06242aa2468916707ec35f36
dion-zebua <zebuadbless@gmail.com>
2025-09-03 14:24:10 +0700

    malas

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}", 'index.html', "index.php"],
+  theme: {
+    screens: {
+      'sm': '576px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    },
+    fontFamily: {
+      poppins: "Poppins",
+    },
+    extend: {
+      colors: {
+        'primary': '#44ac8d',
+        'primary-hover': '#1e717d',
+      }
+    },
+  },
+  plugins: [],
+}
